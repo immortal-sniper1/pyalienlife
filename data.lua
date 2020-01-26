9e0275ee170ef903ac861883faa652b3954c0bb6
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-01-26 06:53:31 -0500

    make 18 branch and fix caravans

--- data.lua
+++ data.lua
@@ -237,7 +237,7 @@ require("prototypes/buildings/trits-reef-mk01")
 require("prototypes/buildings/trits-reef-mk02")
 require("prototypes/buildings/trits-reef-mk03")
 require("prototypes/buildings/trits-reef-mk04")
---require('prototypes/buildings/outpost')
+require('prototypes/buildings/outpost')
 --require('prototypes/buildings/foodbowl')
 require('prototypes/buildings/generator-1')
 require('prototypes/buildings/generator-2')
@@ -252,7 +252,7 @@ require("prototypes/buildings/harvester")
 require("prototypes/buildings/collector")
 
 --Caravan ceature
---require('prototypes/ceatures/caravan')
+require('prototypes/ceatures/caravan')
 
 --Land bot
 --require('prototypes/robots/land-bot')
